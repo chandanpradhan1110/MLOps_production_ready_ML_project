c0535da55a764a3a65515eb394b51a851c138e5a
chandanpradhan1110 <chandan.pradhan1110@gmail.com>
2026-03-18 02:17:18 +0530

    logging and Exception Added

---
+++ demo.py
@@ -0,0 +1,11 @@
+from mlProject.logger import logging
+from mlProject.exception import USvisaException
+import sys
+
+
+logging.info("Welcome to our custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e,sys)
