096d0e06a9b309d1301b89458bc2fa88021d74e2
chandanpradhan1110 <chandan.pradhan1110@gmail.com>
2026-03-18 00:53:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlProject",
+    version="0.0.0",
+    author="Chandan",
+    author_email="chandan.pradhan1110@gmail.com",
+    packages=find_packages()
+)
