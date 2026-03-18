688ba78f8ee35ac5e7282da94f18a32316537146
chandanpradhan1110 <chandan.pradhan1110@gmail.com>
2026-03-18 14:02:41 +0530

    Project commited

--- demo.py
+++ demo.py
@@ -1,11 +1,16 @@
-from mlProject.logger import logging
-from mlProject.exception import USvisaException
-import sys
+# from mlProject.logger import logging
+# from mlProject.exception import USvisaException
+# import sys
 
 
-logging.info("Welcome to our custom log")
+# logging.info("Welcome to our custom log")
 
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+# try:
+#     a = 2/0
+# except Exception as e:
+#     raise USvisaException(e,sys)
+
+from mlProject.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
